9b2b1516daf11d66ada0ae72bc8058852522bf74
bvelez <Bienvenido.Velez@upr.edu>
2016-08-18 13:01:46 -0400

    Second version of Lecture 1

--- Lecture1/src/MainClass.java
+++ Lecture1/src/MainClass.java
@@ -1,24 +1,17 @@
 import java.util.Scanner;
 
-
 class MainClass {
-	public static int MU = 0;
+
 	public static void main(String[] args) {
 
+		double PI = 3.14;
+
 		Scanner in = new Scanner(System.in);
-
-		double PI = 3.14, area, ci;
-		
-		System.out.println(MU);
-		
 		System.out.println("Enter radius of circle: ");
-		double rad = in.nextDouble();
-		
-		area = Math.PI * rad * rad;
-		System.out.println("Area of circle = " + area);
+		double radius = in.nextDouble();
 
-		ci = 2.0 * PI * rad;
-		System.out.println("Circumference = " + ci);
+		double area = PI * radius * radius;
+		System.out.println("Area of circle = " + area);
 
 		in.close();
 	}
